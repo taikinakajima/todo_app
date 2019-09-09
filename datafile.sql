7ef03e10f8a907ffc119ed7bf7bb4fcf1239a25b
taikinakajima <nandrop1109@gmail.com>
2019-09-09 16:48:05 +0900

    clean_commit

--- datafile.sql
+++ datafile.sql
@@ -1,15 +1,17 @@
-CREATE TABLE users (
- id SERIAL NOT NULL PRIMARY KEY ,
- name VARCHAR( 25 ) NOT NULL ,
- email VARCHAR( 35 ) NOT NULL ,
- password VARCHAR( 60 ) NOT NULL ,
- UNIQUE (email)
+CREATE TABLE users
+(
+    id SERIAL NOT NULL PRIMARY KEY ,
+    name VARCHAR( 25 ) NOT NULL ,
+    email VARCHAR( 35 ) NOT NULL ,
+    password VARCHAR( 60 ) NOT NULL ,
+    UNIQUE (email)
 );
 
-CREATE TABLE posts (
- id SERIAL NOT NULL PRIMARY KEY ,
- user_id int NOT NULL references users(id) ,
- content VARCHAR( 100 ) NOT NULL ,
- created_at date NOT NULL,
- uploaded_at date  NOT NULL
+CREATE TABLE posts
+(
+    id SERIAL NOT NULL PRIMARY KEY ,
+    user_id int NOT NULL references users(id) ,
+    content VARCHAR( 100 ) NOT NULL ,
+    created_at date NOT NULL,
+    uploaded_at date NOT NULL
 );
